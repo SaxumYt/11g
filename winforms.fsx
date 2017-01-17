157b7438a32b2828a9dade9bc9440ee4f5f26728
Balder I <balderi@tsponline.dk>
2017-01-17 17:04:01 +0100

    masser af gode ting

--- winforms.fsx
+++ winforms.fsx
@@ -1,42 +1,80 @@
 open System.Windows.Forms
 open System.Drawing
 
+let timer = new Timer()
+let butan = new Button()
 
-let winHeight = 600
-let winWidth = 600
-let origo = (winHeight/2, winWidth/2)
-let pos = 
-  [[100;200];
-  [100;300];
-  [100;400];
-  ]
-(*
-let updatePoints (form : Form) (coords : polyline byref) dtheta center showtime =
-  let centeredCoords = translatePoints (scalePoint -1.0 center) coords
-  let rotatedCoords = rotatePoints dtheta centeredCoords
-  coords <- translatePoints center rotatedCoords
-  form.Refresh ()
-*)
+/// Create a form and add a paint function
+let createForm backgroundColor (width, height) title =
+  let win = new Form ()
+  win.Text <- title
+  win.BackColor <- backgroundColor
+  win.ClientSize <- Size (width, height)
+  win
 
-let win = new Form ()
-win.Text <- "title"
-win.BackColor <- Color.White
-win.ClientSize <- Size (winHeight, winWidth)
-
-
+// Setup drawing details
+let title = "title"
+let backgroundColor = Color.Black
+let size = (800, 800)
+let win = createForm backgroundColor size title
+let mutable planetCoordX = 0
+let mutable planetCoordY = 0
+let mutable tickCounter = 0
+let mutable maxRuns = 0   // 0 to Int32
 
 let drawPlanets() =
-   win.Paint.Add (fun e -> e.Graphics.DrawEllipse (new Pen ((Color.Blue), 5.0F),400,400,5,5))  
-   win.Paint.Add (fun e -> e.Graphics.DrawEllipse (new Pen ((Color.Red), 5.0F),300,300,5,5))  
-let updatePlanets() = 
-  win.Refresh()
+    win.Paint.Add (fun e -> e.Graphics.DrawEllipse (new Pen ((Color.LightGray), 2.0F),planetCoordX+4,planetCoordY+4,2,2)) //Mercury
+    win.Paint.Add (fun e -> e.Graphics.DrawEllipse (new Pen ((Color.Goldenrod), 2.0F),planetCoordX+7,planetCoordY+7,2,2)) //Venus
+    win.Paint.Add (fun e -> e.Graphics.DrawEllipse (new Pen ((Color.CornflowerBlue), 2.0F),planetCoordX+9,planetCoordY+9,2,2)) //Earf
+    win.Paint.Add (fun e -> e.Graphics.DrawEllipse (new Pen ((Color.Maroon), 2.0F),planetCoordX+14,planetCoordY+14,2,2)) //Mars
+    win.Paint.Add (fun e -> e.Graphics.DrawEllipse (new Pen ((Color.LightCoral), 5.0F),planetCoordX+51,planetCoordY+51,5,5)) //Jupiter
+    win.Paint.Add (fun e -> e.Graphics.DrawEllipse (new Pen ((Color.DarkKhaki), 5.0F),planetCoordX+90,planetCoordY+90,5,5)) //Saturn
+    win.Paint.Add (fun e -> e.Graphics.DrawEllipse (new Pen ((Color.LightSkyBlue), 5.0F),planetCoordX+180,planetCoordY+180,3,3)) //Uranus
+    win.Paint.Add (fun e -> e.Graphics.DrawEllipse (new Pen ((Color.RoyalBlue), 5.0F),planetCoordX+300,planetCoordY+300,3,3)) //Neptune
+    win.Paint.Add (fun e -> e.Graphics.DrawEllipse (new Pen ((Color.Tan), 2.0F),planetCoordX+320,planetCoordY+320,2,2)) //Pluto
+
+let tbTime = new TextBox()
+tbTime.Width <- 100
+tbTime.Height <- 50
+tbTime.Visible <- true
+tbTime.Enabled <- true
+tbTime.Location <- new Point (10, 10)
+win.Controls.Add tbTime
+
+let clicky (e : System.EventArgs) =
+    maxRuns <- ((int)tbTime.Text * 10)
+    timer.Enabled <- true
+    butan.Enabled <- false
+    tbTime.Enabled <- false
+
+butan.Text <- "GO!"
+butan.Height <- 50
+butan.Width <- 50
+butan.Location <- new Point (10, 50)
+butan.ForeColor <- Color.White
+butan.Click.Add clicky
+win.Controls.Add butan
+
+
+let updatePlanets (form : Form) timerEventMagic =
+    planetCoordX <- planetCoordX + 1
+    tickCounter <- tickCounter + 1
+    if maxRuns = 0 then
+        ()
+    elif tickCounter >= maxRuns then
+        timer.Enabled <- false
+        butan.Enabled <- true
+        tbTime.Enabled <- true
+        tickCounter <- 0
+    form.Refresh()
 
 // Add timer events
-let timer = new Timer()
-timer.Interval <- 1000
-timer.Enabled <- true  
-timer.Tick.Add (updatePlanets())
+timer.Interval <- 100
+timer.Enabled <- false
+timer.Tick.Add (updatePlanets win)
 
 
+
+drawPlanets()
 
 Application.Run win
