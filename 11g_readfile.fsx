8f29ecbedc0573cac4386849fc998699b04a9510
SaxumYt <Saxumyt@gmail.com>
2017-01-17 11:08:09 +0100

    bruger vector typen men der er nogle fejl jeg ikke har løst

--- 11g_readfile.fsx
+++ 11g_readfile.fsx
@@ -1,19 +1,22 @@
-type Vector (x, y, z) = class
+type Vector (x:float, y:float, z:float) = class
     static member (+) (vector1: Vector, vector2: Vector)=
-        (vector1.x + vector2.x, vector1.y + vector2.y, vector1.z + vector2.z)
+        Vector (vector1.x + vector2.x, vector1.y + vector2.y, vector1.z + vector2.z)
+    static member (-) (vector1: Vector, vector2: Vector)=
+        Vector (vector1.x - vector2.x, vector1.y - vector2.y, vector1.z - vector2.z)
     static member ( * ) (a, vector: Vector)=
-        (vector.x*a, vector.y*a, vector.z*a)
+        Vector (vector.x*a, vector.y*a, vector.z*a)
     static member ( * ) (vector1: Vector, vector2: Vector)=
-        (vector1.x * vector2.x, vector1.y * vector2.y, vector1.z * vector2.z)
-    static member absoluteVal (vector) =
-        sqrt((power vector.x 2) + (power vector.y 2) + (power vector.z 2))
+        Vector (vector1.x * vector2.x, vector1.y * vector2.y, vector1.z * vector2.z)
+    member this.absoluteVal() =
+        sqrt(float (pown (this.x) 2) + float (pown (this.y) 2) + float (pown (this.z) 2))
+    member this.tupleConvert() =
+        (this.x, this.y, this.z)
     member this.x = x
     member this.y = y
     member this.z = z
 
 
 end
-
 
 
 
@@ -26,38 +29,59 @@ let SphericalToCartesian (long:float, lat:float, r:float) =
   let x = r*sin(degToRad(lat+90.0))*cos(degToRad(long))
   let y = r*sin(degToRad(lat+90.0))*sin(degToRad(long))
   let z = r*cos(degToRad(lat+90.0))
-  (x,y,z)
+  Vector (x,y,z)
 
 type planet() =
-    let mutable route = []
+    let mutable route: Vector list = []
     let mutable pos = 0
+    let mutable delta = 0.1
+    member this.Delta
+        with get() = delta
+        and set(value) = delta <- value
     member this.setRoute(newroute) = route <- newroute
-    member this.showRoute() = printfn "Planets route:\n %A" route
-    member this.calculateSpeed() =
+    member this.showRoute() =
+        for vector in route do
+            printfn "%A" (vector.tupleConvert())
+    member this.calculateStartSpeed() =
        match (route.[pos], route.[pos+1]) with
-       | ((x1,y1,z1),(x2,y2,z2)) -> printfn "%A" (scale*(    sqrt ( (pown (x2-x1) 2)   +    (pown (y2-y1) 2)  + (pown (z2-z1) 2) )  ))
+       | (vector1: Vector, vector2: Vector) ->(((vector1 - vector2).absoluteVal())*scale)
+
+    member this.acceleration (t:float) =
+        //-1*(((0.0002959122)/(pown (this.position(t).absoluteVal) 3))*this.position)
+        ()
+
+    member this.position (t:float) =
+        match t with
+        | y when y<0.0 -> route.[0]
+        | x -> this.position(t-delta) + this.velocity(t-delta)*delta
+
+    member this.velocity (t:float) =
+        match t with
+        | y when y<0.0 -> this.calculateStartSpeed()
+        | x ->
+            this.velocity + this.acceleration(x-delta)*delta
+
 
 
        // bestem afstand her
     member this.test() =
-       printfn "%A\n%A\n%A\n%A" route.[0] route.[90] route.[180] route.[270]
+       printfn "%A\n%A\n%A\n%A" (route.[0].tupleConvert()) (route.[90].tupleConvert()) (route.[180].tupleConvert()) (route.[270].tupleConvert())
 
     member this.loadDataFromFile (filename: string, coords:int) =
         let reader = System.IO.File.OpenText filename
         let mutable data = []
+        let mutable cleandata = []
         while not(reader.EndOfStream) do
             if reader.ReadLine () = "$$SOE" then
                   // antal koordinater der gemmes
                   for i=0 to coords-1 do
-                      let mutable cleandata = []
+                      cleandata <- []
                         // laver string til liste
                       let linje = reader.ReadLine().Split ' '
                       for value in linje do
                         // fjerner tomme elementer
                         if value.Length <> 0 then
                             cleandata <- cleandata @ [value]
-                        else
-                           ()
                       // konverterer data og gemmer i listen  FILFORMAT: TID LONG LAT R
 
                       //printfn "%A %A %A %A" (float cleandata.[1]) cleandata.[2] cleandata.[3]
@@ -84,7 +108,7 @@ type planet() =
 //dataToRoute (Pluto, "data/Pluto.txt", 1500)
 
 let Earth = new planet()
-Earth.loadDataFromFile("data/Earth.txt", 500)
+Earth.loadDataFromFile("Earth.txt", 500)
 
 Earth.showRoute()
 //Earth.calculateSpeed()
